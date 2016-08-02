da321863066fd4c6e886c84c3ca633a0fd0a9750
Ben Haan <benhaan@gmail.com>
2016-08-02 17:08:01 -0500

    pass in additional options

--- lib/lc_api/series.rb
+++ lib/lc_api/series.rb
@@ -5,8 +5,13 @@ module LcApi
     def find_image(name)
       (self.images.detect {|f| f.name.downcase == name.to_s.downcase})
     end
-    def current
-    	Series.get(:current)
+
+    def self.current(*args)
+      options = args.slice!(0) || {}
+      options[:params] = (options[:params] || {}).merge({include: options[:include].join(",")}) if options.has_key? :include
+      options[:params] = (options[:params] || {}).merge({key: LcApi.key})
+
+    	Series.get(:current, options[:params])
     end
   end
 end
